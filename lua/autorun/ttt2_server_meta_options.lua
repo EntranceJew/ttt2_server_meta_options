524be5acd5a934989aa19ad450f603a49441d45b
EntranceJew <EntranceJew@gmail.com>
2023-08-28 01:01:03 -0500

    server meta + benj stuff

---
+++ lua/autorun/ttt2_server_meta_options.lua
@@ -0,0 +1,5 @@
+if SERVER then
+	AddCSLuaFile()
+
+	include("ttt2_server_meta_options/server/sv_ttt2_server_meta_options.lua")
+end
---
+++ lua/terrortown/lang/ttt2_server_meta_options.lua
@@ -0,0 +1,25 @@
+L = LANG.GetLanguageTableReference("en")
+
+-- hee hoo
+
+L["server_meta_options_addon_info"] = "Extra Lootable Props"
+
+L["server_meta_options_settings_general"] = "General"
+
+L["label_ttt2_sv_smo_enabled"] = "Extra Lootable Props Enabled"
+L["help_ttt2_sv_smo_enabled"] = "Whether broken props should have a chance at spawning guns or ammo."
+
+L["label_ttt2_sv_smo_only_appropriate_props"] = "Only Appropriate Props"
+L["help_ttt2_sv_smo_only_appropriate_props"] = "Only allow props with matching names to roll for drops."
+
+L["label_ttt2_sv_smo_prop_name_matches"] = "Prop Name Matches"
+L["help_ttt2_sv_smo_prop_name_matches"] = "The names to match props against.\nSeparate entries with \"|\".\nSearches the path as well the model name."
+
+L["label_ttt2_sv_smo_max_items_per_prop"] = "Max Items Per Prop"
+L["help_ttt2_sv_smo_max_items_per_prop"] = "How many items a prop is capable of dropping if every roll is successful."
+
+L["label_ttt2_sv_smo_drop_chance"] = "Drop Chance"
+L["help_ttt2_sv_smo_drop_chance"] = "The likelyhood that an individual item will drop."
+
+L["label_ttt2_sv_smo_weapon_drop_chance"] = "Weapon Drop Chance"
+L["help_ttt2_sv_smo_weapon_drop_chance"] = "The likelyhood that an item drop will be upgraded from ammo to a full weapon."
